07c550166f2dca27cc41aa773aa61428e318e05a
ayushi-shahi <ayushishaho14072004@gmail.com>
2025-08-22 19:57:58 +0530

    Adding clerk and inngest into application

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,4 +5,13 @@ export const ENV = {
   PORT: process.env.PORT ||5001,
   MONGO_URI : process.env.MONGO_URI ,
   NODE_ENV :process.env.NODE_ENV,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+
 }
+
+//cHRk2TLjAyMmwF8Q
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 const app = express();
  app.get("/" ,(req , res) => {
  res.send("Hello World")
  })
- app.listen(ENV.PORT , () => console.log("Server started on port:" ,ENV.PORT));
+ app.listen(ENV.PORT , () =>{
+   console.log("Server started on port:" ,ENV.PORT)
+   connectDB();
+ });
 
   
